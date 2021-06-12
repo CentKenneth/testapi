1e336327ce7d67886f2b017096175af8bf990350
leonhail <leonhail.paypa@positivegroup.com.au>
2021-06-12 10:13:15 +0700

    fix issues

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        try {
+            $user = User::create([
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('secret')
+            ]);
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+}
